579f177ca56e6fefe4db4a27e7c7f0386dc9afe4
Anisha Yadav <2000anishayadav@gmail.com>
2024-12-18 22:08:55 +0530

    AI blog generater

---
+++ blog_generator/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('login', views.user_login),
+    path('signup', views.signup),
+    path('logout', views.user_logout),
+    
+]
+
